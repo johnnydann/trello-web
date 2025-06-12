413b300d8dabdac2dd9e627dae3ec31456cc9b62
dpn <johnnydang2713@gmail.com>
2025-06-13 02:22:06 +0700

    header  trello

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,18 +1,87 @@
-import Box from '@mui/material/Box';
-import ModeSelect from '../../components/ModeSelect';
+import Box from "@mui/material/Box";
+import ModeSelect from "~/components/ModeSelect";
+import AppIcon from "@mui/icons-material/Apps";
+import { ReactComponent as TrelloIcon } from "~/assets/mdi--trello.svg";
+import SvgIcon from "@mui/material/SvgIcon";
+import Typography from "@mui/material/Typography";
+import Workspaces from "./Menus/Workspaces";
+import Recent from "./Menus/Recent";
+import Starred from "./Menus/Starred";
+import Templates from "./Menus/Templates";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Badge from "@mui/material/Badge";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import Tooltip from "@mui/material/Tooltip";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import Profiles from "./Menus/Profiles";
 
 function AppBar() {
-  return (
-    <Box sx={{
-        backgroundColor: 'primary.light',
-        width: '100%',
-        height: (theme) => theme.trello.appBarHeight,
-        display: 'flex',
-        alignItems: 'center',
-    }}>
-        <ModeSelect />
-    </Box>
-  )
+    return (
+        <Box
+            px={2}
+            sx={{
+                width: "100%",
+                height: (theme) => theme.trello.appBarHeight,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+            }}
+        >
+            <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+                <AppIcon sx={{ color: "primary.main" }} />
+                <Box sx={{ display: "flex", alignItems: "center", gap: 0.5 }}>
+                    <SvgIcon
+                        component={TrelloIcon}
+                        fontSize='small'
+                        inheritViewBox
+                        sx={{ color: "primary.main" }}
+                    />
+                    <Typography
+                        variant='span'
+                        sx={{
+                            fontSize: "1.2rem",
+                            fontWeight: "bold",
+                            color: "primary.main",
+                        }}
+                    >
+                        Trello
+                    </Typography>
+                </Box>
+                <Workspaces />
+                <Recent />
+                <Starred />
+                <Templates />
+
+                <Button variant='outlined'>Create</Button>
+            </Box>
+            <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+                <TextField
+                    size='small'
+                    id='outlined-search'
+                    label='Search field'
+                    type='search'
+                />
+                <ModeSelect />
+                <Tooltip title='Notification'>
+                    <Badge
+                        color='secondary'
+                        variant='dot'
+                        sx={{ cursor: "pointer" }}
+                    >
+                        <NotificationsNoneIcon sx={{ color: "primary.main" }} />
+                    </Badge>
+                </Tooltip>
+                <Tooltip title='Help'>
+                    <HelpOutlineIcon
+                        sx={{ cursor: "pointer", color: "primary.main" }}
+                    />
+                </Tooltip>
+
+                <Profiles />
+            </Box>
+        </Box>
+    );
 }
 
-export default AppBar
+export default AppBar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
-import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
-
-import theme from './theme';
+import CssBaseline from "@mui/material/CssBaseline";
+import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles";
+import theme from "./theme";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
