3ea18e849d39d130a5ab3811ed908b129d798824
dpn <johnnydang2713@gmail.com>
2025-09-04 18:13:22 +0700

    delete clg

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,5 @@
-// export const API_ROOT = 'http://localhost:5000'
 let apiRoot = ''
-console.log('process.env: ', process.env)
+// console.log('process.env: ', process.env)
 if (process.env.BUILD_MODE === 'dev') {
     apiRoot = 'http://localhost:5000'
 }
@@ -9,6 +8,6 @@ if (process.env.BUILD_MODE === 'production') {
     apiRoot = 'https://trello-backend-y8vj.onrender.com'
 }
 
-console.log('🚀 ~ apiRoot:', apiRoot)
+// console.log('🚀 ~ apiRoot:', apiRoot)
 
 export const API_ROOT = apiRoot
